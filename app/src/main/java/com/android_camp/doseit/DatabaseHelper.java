18f71acf2139538ee844981a94cd1aaa7c487e94
madeleine789 <madeleine789@gmail.com>
2016-08-04 11:52:45 -0700

    Restored DatabaseHelper class

---
+++ app/src/main/java/com/android_camp/doseit/DatabaseHelper.java
@@ -0,0 +1,48 @@
+package com.android_camp.doseit;
+import android.util.Log;
+
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBaseHelper {
+    private Firebase ref;
+    public interface Help {
+        List<Medicine> dealWithData(List<Medicine> l);
+    }
+    public void initDataBase(final Help h) {
+        Firebase.getDefaultConfig().setPersistenceEnabled(true);
+        ref = new Firebase("https://doseit-f8672.firebaseio.com/");
+        if(ref != null) {
+            ref.keepSynced(true);
+            ref.child("medicine").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    ArrayList<Medicine> med = new ArrayList<Medicine>((int) snapshot.getChildrenCount());
+                    for(DataSnapshot child: snapshot.getChildren()) {
+                        System.out.println(child);
+                        Medicine current = new Medicine();
+                        current.setName((String) child.child("name").getValue());
+                        current.setWariningMessage((String) child.child("warning").getValue());
+                        current.setConcentration(Double.parseDouble((String) child.child("concentration").getValue()));
+                        current.setKidDose(Double.parseDouble((String) child.child("pedidose").getValue()));
+                        current.setDose(Double.parseDouble((String) child.child("dose").getValue()));
+                        med.add(current);
+                    }
+                    h.dealWithData(med);
+                }
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+                    Log.d(this.getClass().getName(), "The read failed: " + firebaseError.getMessage());
+                }
+            });
+        }
+        else {
+            Log.d(this.getClass().getName(), "Internet connection is needed to get the database for the first time");
+        }
+    }
+}
