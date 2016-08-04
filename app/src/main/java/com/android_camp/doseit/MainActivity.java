6df63e24d0984de3acc4ff68475cdbddaef86375
nsfxc <nnssffxxcc@gmail.com>
2016-08-04 09:13:12 +0100

    git test

--- app/src/main/java/com/android_camp/doseit/MainActivity.java
+++ app/src/main/java/com/android_camp/doseit/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+//little test of git
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
