09f29abcb8bc2b33d69512e1739f070f5f7ca107
nsfxc <nnssffxxcc@gmail.com>
2016-08-05 06:21:56 +0100

    Alert for photo mode

--- app/src/main/java/com/android_camp/doseit/MainActivity.java
+++ app/src/main/java/com/android_camp/doseit/MainActivity.java
@@ -1,7 +1,11 @@
 package com.android_camp.doseit;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import com.firebase.client.Firebase;
@@ -19,14 +23,42 @@ public class MainActivity extends BaseActivity {
         initToolBar();
     }
 
+    public void cameraAlert(){
+
+        Log.d("MSG", "clicked");
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("Sorry");
+        alertDialog.setMessage("This function has not been implemented yet, comming soon.");
+        alertDialog.setIcon(R.drawable.ic_camera_alt_black_24dp);
+
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Can't wait to see it.",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "It dosen't matter.",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+
     public void onClickBtn(View view) {
+        Log.d("MSG", "clicked");
+
         Intent intent = null;
         switch (view.getId()) {
             case R.id.text_search_btn:
                 intent = new Intent(this, SwipeActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.photo_search_btn:
+                cameraAlert();
                 break;
         }
-        startActivity(intent);
     }
 
 }
